01e57baf6b49a0ae380ff655981e8c200d87463a
bntufte <bntufte@outlook.com>
2021-01-03 23:11:09 -0800

    added full repo

--- src/main/java/com/trivia/models/User.java
+++ src/main/java/com/trivia/models/User.java
@@ -1,5 +1,8 @@
 package com.trivia.models;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -7,6 +10,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 
@@ -17,28 +21,105 @@ public class User {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name="user_id")
-	private int userid;
+	private int userId;
+	
+	@Column
 	private String username;
+	@Column
 	private String password;
 	
-	public int getUserid() {
-		return userid;
+	@OneToMany(mappedBy ="user", fetch = FetchType.EAGER)
+	private List<Score> userScores = new ArrayList<>();
+	
+	@OneToOne(mappedBy = "user_id", fetch = FetchType.EAGER)
+	private Showcase showcase;
+
+	public User() {
+		super();
 	}
-	public void setUserid(int userid) {
-		this.userid = userid;
+
+	public User(String username, String password, List<Score> userScores, Showcase showcase) {
+		super();
+		this.username = username;
+		this.password = password;
+		this.userScores = userScores;
+		this.showcase = showcase;
 	}
+
+	public User(int userId, String username, String password, List<Score> userScores, Showcase showcase) {
+		super();
+		this.userId = userId;
+		this.username = username;
+		this.password = password;
+		this.userScores = userScores;
+		this.showcase = showcase;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
 	public String getUsername() {
 		return username;
 	}
+
 	public void setUsername(String username) {
 		this.username = username;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
-	
 
+	public List<Score> getUserScores() {
+		return userScores;
+	}
+
+	public void setUserScores(List<Score> userScores) {
+		this.userScores = userScores;
+	}
+
+	public Showcase getShowcase() {
+		return showcase;
+	}
+
+	public void setShowcase(Showcase showcase) {
+		this.showcase = showcase;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(password, showcase, userId, userScores, username);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		User other = (User) obj;
+		return Objects.equals(password, other.password) && Objects.equals(showcase, other.showcase)
+				&& userId == other.userId && Objects.equals(userScores, other.userScores)
+				&& Objects.equals(username, other.username);
+	}
+
+	@Override
+	public String toString() {
+		return "User [userId=" + userId + ", username=" + username + ", password=" + password + ", userScores="
+				+ userScores + ", showcase=" + showcase + "]";
+	}	
 }
---
+++ src/main/java/com/trivia/repos/ScoreDAOPostgres.java
@@ -0,0 +1,53 @@
+package com.trivia.repos;
+
+import java.util.List;
+
+import javax.persistence.criteria.CriteriaQuery;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.trivia.models.Score;
+
+@Repository
+@Transactional
+public class ScoreDAOPostgres implements ScoreDAO{
+
+	@Autowired
+	private SessionFactory sf;
+	
+	@Override
+	public Score findById(int id) {
+		Session s = sf.getCurrentSession();
+		return s.get(Score.class, id);
+	}
+
+	@Override
+	public List<Score> findAll() {
+		Session s = sf.getCurrentSession();
+		CriteriaQuery<Score> cq = s.getCriteriaBuilder().createQuery(Score.class);
+		cq.from(Score.class);
+		return s.createQuery(cq).getResultList();
+	}
+
+	@Override
+	public void insert(Score s) {
+		Session ses = sf.getCurrentSession();
+		ses.saveOrUpdate(s);
+	}
+
+	@Override
+	public void delete(Score s) {
+		Session ses = sf.getCurrentSession();
+		ses.update(s);
+	}
+
+	@Override
+	public void update(Score s) {
+		Session ses = sf.getCurrentSession();
+		ses.delete(s);
+	}
+}
