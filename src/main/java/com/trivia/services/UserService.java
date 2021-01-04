f8bae71fe99d088ba01383657f40f4e4b003b6e8
bntufte <bntufte@outlook.com>
2021-01-04 02:02:56 -0800

    added basic services

---
+++ src/main/java/com/trivia/services/UserService.java
@@ -0,0 +1,54 @@
+package com.trivia.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.trivia.models.User;
+import com.trivia.repos.UserDAO;
+
+@Service
+public class UserService {
+
+	private UserDAO uDao;
+	
+	@Autowired
+	public UserService(UserDAO uDao) {
+		super();
+		this.uDao = uDao;
+	}
+	
+	public User getById(int id) {
+		return uDao.findById(id);
+	}
+	
+	public User getByName(String name) {
+		return uDao.findByUsername(name);
+	}
+
+	public List<User> getAll() {		
+		return uDao.findAll();
+	}
+
+	public boolean storePokemon(User u) {
+		uDao.insert(u);
+		if(uDao.findById(u.getUserId()).equals(u))
+			return true;
+		return false;
+	}
+
+	public boolean update(User u) {
+		uDao.update(u);
+		if(uDao.findById(u.getUserId()).equals(u))
+			return true;
+		return false;
+	}
+
+	public boolean delete(User u) {
+		uDao.delete(u);
+		if(!uDao.findById(u.getUserId()).equals(u))
+			return true;
+		return false;
+	}	
+}
