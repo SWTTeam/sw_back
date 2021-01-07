01c80a497c2122f91fce80d86f07f1391db39aa9
jlrjr98 <73042836+jlrjr98@users.noreply.github.com>
2021-01-06 19:26:33 -1000

    PersonRewardController change

--- src/main/java/com/trivia/controllers/PersonRewardController.java
+++ src/main/java/com/trivia/controllers/PersonRewardController.java
@@ -26,14 +26,6 @@ public class PersonRewardController {
 		this.personRewardService = personRewardService;
 	}
 	
-	private UserService userService;
-	
-	@Autowired
-	public PersonRewardController(UserService userService) {
-		super();
-		this.userService = userService;
-	}
-	
 	@PostMapping
 	public ResponseEntity insertPersonReward(@RequestBody PersonReward personReward) {
 		
