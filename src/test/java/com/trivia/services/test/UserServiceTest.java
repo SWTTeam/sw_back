92348e5584bd80c3cfdb8a78ecf457558713357d
logriffith <logriffith1991@gmail.com>
2021-01-06 22:09:10 -0700

    worked on JUnit tests

--- src/test/java/com/trivia/services/test/UserServiceTest.java
+++ src/test/java/com/trivia/services/test/UserServiceTest.java
@@ -1,17 +1,20 @@
 package com.trivia.services.test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.trivia.models.PersonReward;
+import com.trivia.models.Score;
 import com.trivia.models.User;
 import com.trivia.services.UserService;
 import com.trivia.utilities.EncryptionUtility;
@@ -50,4 +53,25 @@ public class UserServiceTest {
 //	void getByNameTestNull() {
 //		assertNull(userService.getByName("bntufte22"));
 //	}
+	
+	@Test
+	void loginVerTest() {
+		List<Score> scores = new ArrayList<>();
+		List<PersonReward> rewards = new ArrayList<>();
+		User user = new User("skytsar", "password", scores, rewards, null);
+		assertNotNull(user);
+	}
+	
+	@Test
+	void getAllTest() {
+		assertTrue(userService.getAll().size() != 0);
+	}
+	
+	@Test
+	void updateTest() {
+		List<Score> scores = new ArrayList<>();
+		List<PersonReward> rewards = new ArrayList<>();
+		User user = new User("test", "password55", scores, rewards, null);
+		assertTrue(userService.update(user));
+	}
 }
